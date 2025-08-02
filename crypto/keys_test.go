b64a8787e20f8c91a92cd6cc9c3bea26e64eb23e
fayob <adedejifavour6@gmail.com>
2025-08-02 18:47:39 +0100

    Add Address and more helper functions to create private key

--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -14,6 +14,18 @@ func TestGeneratePrivateKey(t *testing.T)  {
 	assert.Equal(t, len(pubKey.Bytes()), pubKeyLen)
 }
 
+func TestPrivateKeyFromString(t *testing.T) {
+	var (
+		seed = "44ba732b93d3f41c8cf5400c8eb22285d41236545b5fec0dbdebfd03bf07a82b"
+		privKey = NewPrivateKeyFromString(seed)
+		addressStr = "6647fa7ae144122acd2a59f5812bb12752c8a3c6"
+	)
+
+	assert.Equal(t, privKeyLen, len(privKey.Bytes()))
+	address := privKey.Public().Address()
+	assert.Equal(t, address.String(), addressStr)
+}
+
 func TestPrivateKeySignature(t *testing.T) {
 	privKey := GeneratePrivateKey()
 	pubKey := privKey.Public()
@@ -29,4 +41,11 @@ func TestPrivateKeySignature(t *testing.T) {
 	// Test with a different public key
 	invalidPubKey := GeneratePrivateKey().Public()
 	assert.False(t, sig.Verify(invalidPubKey, msg), "Signature should not verify with a different public key")
+}
+
+func TestPublicKeyToAddress(t *testing.T)  {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+	address := pubKey.Address()
+	assert.Equal(t, len(address.value), addressLen, "Address length should match addressLen")
 }
